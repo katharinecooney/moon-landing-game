11de6e4f3462f5d4c7ca80644e8c886181608e26
Katharine Cooney <katharinecooney@gmail.com>
2019-04-18 16:14:42 +0200

    Update background image again to prevent gaps when moving

--- js_files/background.js
+++ js_files/background.js
@@ -9,14 +9,14 @@ function BackgroundImage (canvas){
 
 //Draw Backgroundimage
 BackgroundImage.prototype.draw = function (){
- this.ctx.drawImage(this.img, this.x, 0, this.img.width-200, this.canvas.height);
- this.ctx.drawImage(this.img, this.x+this.img.width-200, 0, this.img.width,this.canvas.height);
+ this.ctx.drawImage(this.img, this.x, 0, this.img.width - 200, this.canvas.height);
+ this.ctx.drawImage(this.img, this.x+this.img.width - 200, 0, this.img.width,this.canvas.height);
 
  };
 
 //move Backgroundimage
 BackgroundImage.prototype.move = function (){
- if(this.x < 0 - this.img.width + 500){
+ if(this.x < 0 - this.img.width + 800){
    this.x = 0
  } else {
    this.x += this.speed;
